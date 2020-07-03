24e2070bc77f66c626ac75d91047b360a1f60309
epicprogramer <67761728+epicprogramer@users.noreply.github.com>
2020-07-03 16:35:15 +0200

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is the main function
+int main(int argc, char const *argv[])
+{
+	printf("%s\n","go beyond do never look back beacouse the besst view comes after the hardes climb!!!!" );
+	
+	return 0;
+}
+
+/*
+Linux Commands line
+ls
+gcc
+./a.out
+rm
+clear
+cd
+ll
+pwd 
+*/
